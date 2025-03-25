2527680b66d9523c748ff117d8517d326ff8176d
iucks <cadastrosrayssa@gmail.com>
2025-03-25 18:00:40 -0300

    Adding assets and a Menu background.

---
+++ code/const.py
@@ -0,0 +1,11 @@
+# C
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('PLAY',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/game.py
+++ code/game.py
@@ -1,9 +1,15 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import pygame
+from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
-        pass
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
